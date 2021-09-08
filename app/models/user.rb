cf9fb48721bedca1398a667037282a3ba3e94f4e
mfrkw <xjdk3766@gmail.com>
2021-09-08 10:44:22 +0000

    [controller追加]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :likes, dependet: :destory
+  # has_many :likes, dependet: :destory
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  
-  resources :books, only: [:index, :create, :destroy, :update]
+  resources :users
+  resources :books
   resource :likes, only:[:create, :destroy]
 end
