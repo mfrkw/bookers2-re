f1113c8edebadf22196aaa6e9c0d2e41cb9fcb7c
mfrkw <xjdk3766@gmail.com>
2021-09-13 09:10:25 +0000

    [book show作成]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,22 +4,26 @@ class BooksController < ApplicationController
     @books =Book.all
   end
   def show
-    @book= Book.find(params:[id])
-    @user= @book.user
-    
+    @book= Book.find(params[:id])
+    @user= @book_user
+
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path
+    if redirect_to book_path(book)
+    else
+      @books = Book.all
+      render index
+    end
   end
-  
+
   def edit
   end
   def update
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
